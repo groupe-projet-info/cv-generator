c7f608d6083812642bf6f2ee498818c27cd3e657
antadama <antaadama@gmail.com>
2022-05-10 09:49:02 +0200

    fix(backend): One line commented to enable running the server

--- backend/app/routes/user.routes.js
+++ backend/app/routes/user.routes.js
@@ -2,7 +2,7 @@ module.exports = app => {
   const users = require("../controllers/user.controller.js");
   var user_router = require("express").Router();
   user_router.post("/", users.create);
-  user_router.post("/password", users.updatePassword);
+  //user_router.post("/password", users.updatePassword);
   user_router.get("/", users.findAll);
   user_router.delete("/:id", users.delete);
   user_router.delete("/", users.deleteAll);
