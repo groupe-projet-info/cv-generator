dac584788924dbaa56bcf7b07775a23694e29807
antadama <antaadama@gmail.com>
2022-05-12 14:00:25 +0200

    Removing auth for testing purposes

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,6 @@ require("./app/routes/skill.routes")(app);
 require("./app/routes/user.routes")(app);
 require("./app/routes/certification.routes")(app);
 require("./app/routes/auth.routes")(app);
-require("./app/routes/template.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
