e09e89c9355b7ed8a38bcf86f7441440169401b7
antadama <antaadama@gmail.com>
2022-05-12 15:23:15 +0200

    Interface Back/Front

--- frontend/plugins/api/certification.ts
+++ frontend/plugins/api/certification.ts
@@ -2,20 +2,19 @@ import { NuxtAxiosInstance } from "@nuxtjs/axios"
 import { Store } from 'vuex'
 
 export interface CertificationAPI {
-  getList(cvId: any): void,
-  getItem(cvId: any, id: any): void,
-  create(cvId: any): Promise<any>,
-  delete(cvId: any, id: any): Promise<boolean>,
-  update(cvId: any, id: any): Promise<boolean>,
+  getList(cvId: any): Promise<any>,
+  add_to_cv(cvId: any): Promise<any>,
+  deleteItem(cvId: any, id: any): Promise<boolean>,
+  deleteList(cvId: any): Promise<boolean>
+
 }
 
 function generateCertification($axios: NuxtAxiosInstance, store: Store<any>): CertificationAPI {
   return {
     async getList(cvId: any) { },
-    async getItem(cvId: any, id: any) { },
-    async create(cvId: any) { return '0' },
-    async delete(cvId: any, id: any) { return false },
-    async update(cvId: any, id: any) { return false },
+    async add_to_cv(cvId: any) { },
+    async deleteItem(cvId: any, id: any) { return false },
+    async deleteList(cvId: any) { return false }
   }
 }
 
--- frontend/plugins/api/index.ts
+++ frontend/plugins/api/index.ts
@@ -5,12 +5,23 @@ import { default as auth, AuthAPI } from './auth'
 import { default as cv, CVAPI } from './cv'
 import { default as user, UserAPI } from './user'
 import { default as template, TemplateAPI } from './template'
+import { default as certification, CertificationAPI  } from './certification'
+import { default as education, EducationAPI } from './education'
+import { default as job, JobAPI } from './job'
+import { default as language, LanguageAPI } from './language'
+import { default as skill, SkillAPI } from './skill'
+
 
 interface API {
   auth: AuthAPI,
   cv: CVAPI,
   user: UserAPI,
   template: TemplateAPI
+  certification: CertificationAPI
+  education: EducationAPI
+  job: JobAPI
+  language: LanguageAPI
+  skill: SkillAPI
 }
 
 declare module 'vue/types/vue' {
@@ -47,7 +58,12 @@ function generateAPI($axios: NuxtAxiosInstance, store: Store<any>): API {
     auth: auth($axios, store),
     cv: cv($axios, store),
     user: user($axios, store),
-    template: template($axios, store)
+    template: template($axios, store),
+    certification: certification($axios, store),
+    education: education($axios, store),
+    job: job($axios, store),
+    language: language($axios, store),
+    skill: skill($axios, store)
   }
 }
 
