ae999207fbec921bade6498fcc69068260fb868f
ayabarradine <aya.ber20@gmail.com>
2022-05-12 16:29:49 +0200

    feat (backend): Adding cv modifying functions and attributes to cv

--- backend/app/routes/cv.routes.js
+++ backend/app/routes/cv.routes.js
@@ -6,13 +6,24 @@ module.exports = app => {
 
     //Getters
     cv_router.get("/:cv_id", cvs.find_one_cv);
+    // Get all the cv of a user
+    cv_router.get("/cvs/:user_id/", cvs.find_user_cvs);
 
     //Modifications
+    cv_router.post("/:cv_id/sex", cvs.set_sex);
+    cv_router.post("/:cv_id/phonenumber", cvs.set_phoneNumber);
+    cv_router.post("/:cv_id/emailadress", cvs.set_emailAdress);
+    cv_router.post("/:cv_id/homeadress", cvs.set_homeAdress);
+    cv_router.post("/:cv_id/drivinglicence", cvs.set_drivingLicence);
     cv_router.post("/:cv_id/hobby", cvs.set_hobbies);
     cv_router.post("/:cv_id/jobtitle", cvs.set_jobtitle);
     
     //Delete
     cv_router.delete("/:cv_id/", cvs.remove_one_cv);
+   
+
+    // Delete user cv 
+    cv_router.delete("/user/:user_id/cv/:cv_id", cvs.delete_user_cv);
 
     app.use("/api/cv",cv_router);
   };
