52e76a9e5f2ba71b76d2ae84ed292edafbb68c05
ayabarradine <aya.ber20@gmail.com>
2022-05-12 22:26:59 +0200

    feat(chore): Interface entre front et back : Skill & Language

--- frontend/plugins/api/language.ts
+++ frontend/plugins/api/language.ts
@@ -2,18 +2,48 @@ import { NuxtAxiosInstance } from "@nuxtjs/axios"
 import { Store } from 'vuex'
 
 export interface LanguageAPI {
-  getList(cvId: any): Promise<any>,
-  add_to_cv(cvId: any): Promise<any>,
-  deleteItem(cvId: any, id: any): Promise<boolean>,
-  deleteList(cvId: any): Promise<boolean>
+  getList(cvId: string): Promise<any>,
+  add_to_cv(cvId: string, lanuageName: string, level: string): Promise<any>,
+  deleteItem(cvId: string, id: any): Promise<boolean>,
+  deleteList(cvId: string): Promise<boolean>
 }
 
 function generateLanguage($axios: NuxtAxiosInstance, store: Store<any>): LanguageAPI {
   return {
-    async getList(cvId: any) { },
-    async add_to_cv(cvId: any) { return '0' },
-    async deleteItem(cvId: any, id: any) { return false },
-    async deleteList(cvId: any) { return false }
+    async getList(cvId: string) {
+      try {
+        const data = await $axios.$get("/api/cv/"+cvId+"/language");
+        return data;
+      } catch (err) {
+        return false
+      }
+    },
+
+    async add_to_cv(cvId: string, lanuageName: string, level: string) {
+      try {
+        let a = await $axios.$post("/api/cv/"+cvId+"/language", {lanuageName: lanuageName, level: level });
+        return true;
+      } catch (_err) {
+        return false
+      }
+    },
+
+    async deleteItem(cvId: string, languageId: string) {
+      try {
+        let a = await $axios.$delete("/api/cv/"+cvId+"/language/"+languageId);
+        return true;
+      } catch (_err) {
+        return false
+      }
+    },
+    async deleteList(cvId: string) {
+      try {
+        let a = await $axios.$delete("/api/cv/"+cvId+"/language/");
+        return true;
+      } catch (_err) {
+        return false
+      }
+    }
   }
 }
 
--- frontend/plugins/api/skill.ts
+++ frontend/plugins/api/skill.ts
@@ -3,18 +3,51 @@ import { Store } from 'vuex'
 
 export interface SkillAPI {
   getList(cvId: any): Promise<any>,
-  add_to_cv(cvId: any): Promise<any>,
-  deleteItem(cvId: any, id: any): Promise<boolean>,
-  deleteList(cvId: any): Promise<boolean>
+  add_to_cv(cvId: string, skillTitle: string, skillValue: [string]): Promise<any>,
+  deleteItem(cvId: String, skillId: String): Promise<boolean>,
+  deleteList(cvId: string): Promise<boolean>
 }
+
 
 function generateSkill($axios: NuxtAxiosInstance, store: Store<any>): SkillAPI {
   return {
-    async getList(cvId: any) { },
-    async add_to_cv(cvId: any) { return '0' },
-    async deleteItem(cvId: any, id: any) { return false },
-    async deleteList(cvId: any) { return false }
+    async getList(cvId: String) {
+      try {
+        const data = await $axios.$get("/api/cv/"+cvId+"/skill");
+        return data;
+      } catch (err) {
+        return false
+      }
+     },
+    
+    
+    async add_to_cv(cvId: string, skillTitle: string, skillValue: [string]) {
+      try {
+        let a = await $axios.$post("/api/cv/"+cvId+"/skill", { skillTitle: skillTitle, skillValue: skillValue});
+        return true;
+      } catch (_err) {
+        return false
+      }
+    },
+
+    async deleteItem(cvId: string, skillId: string) {
+      try {
+        let a = await $axios.$delete("/api/cv/"+cvId+"/skill/"+skillId);
+        return true;
+      } catch (_err) {
+        return false
+      }
+    },
+
+    async deleteList(cvId: string) {
+      try {
+        let a = await $axios.$delete("/api/cv/"+cvId+"/skill/");
+        return true;
+      } catch (_err) {
+        return false
+      }
+    }
   }
-}
+}    
 
 export default generateSkill
