fd121b6fd2b0d14bb2b0f78d23817f5b0c21d4c8
waytostars <palexconso@gmail.com>
2022-05-13 15:02:30 +0200

    chore(frontend): Ajout du fichier manquant au commit

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -68,14 +68,8 @@
       </v-row>
     </v-parallax>
     <v-carousel cycle height="600" hide-delimiter-background show-arrows-on-hover>
-      <v-carousel-item v-for="(slide, i) in slides" :key="i">
-        <v-sheet :color="colors[i]" height="100%">
-          <v-row class="fill-height" align="center" justify="center">
-            <div class="text-h2">
-              {{ slide }} Slide
-            </div>
-          </v-row>
-        </v-sheet>
+      <v-carousel-item>
+        <v-img src="template-default.png"></v-img>
       </v-carousel-item>
     </v-carousel>
   </div>
